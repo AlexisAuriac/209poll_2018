76fd83ec4208273e050c72f02c7504388af234ab
alexis <alexis.auriac@epitech.eu>
2019-05-15 09:58:49 +0200

    The arguments are now displayed correctly

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,16 @@ module Main where
 
 import System.Exit
 import Control.Exception
+import Text.Printf
 
 import ProcessArgv
 
 poll :: IO ()
 poll = do
     args <- processArgv
-    print args
+    printf "Population size:\t%d\n" (argSizeP args)
+    printf "Sample size:\t\t%d\n" (argSizeS args)
+    printf "Voting intentions:\t%.2f%%\n" (argP args)
 
 main :: IO ()
 main = do
