e2e5fbfc16d09a1aa4387be411377f981b50d783
alexis <alexis.auriac@epitech.eu>
2019-05-22 01:53:39 +0200

    Corrected a illogical condition in the error handling

--- src/ProcessArgv.hs
+++ src/ProcessArgv.hs
@@ -22,16 +22,15 @@ processArgv = do
     argv <- getArgs
     progName <- getProgName
     return $ processArgv' argv progName
-processArgv' :: [String] -> String -> Arguments
 
+processArgv' :: [String] -> String -> Arguments
 processArgv' argv progName
     | length argv /= 3 = error $ usage progName
     | (not (isUInt (argv !! 0))) || sizeP == 0 = error "pSize must be an unsigned integer superior to 0"
     | (not (isUInt (argv !! 1))) || sizeS == 0 = error "sSize must be an unsigned integer superior to 0"
+    | sizeS > sizeP = error "sSize must be inferior or equal to sizeP"
     | not $ isFloat (argv !! 2) = error "p must be a decimal number"
     | (p < 0) || (p > 100)  = error "p is a percentage, therefore it must be between 0 and 100"
-    | (not (isFloat (argv !! 2))) || sizeS == 0 = error "sSize must an unsigned int superior to 0"
-    | sizeS > sizeP = error "sSize must be inferior or equal to sizeP"
     | otherwise = (sizeP, sizeS, p/100)
     where
         sizeP = read (argv !! 0) :: Int
